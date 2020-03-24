6537719d075c4f43ead8996e2eb44ed9e6791e31
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-03-24 10:47:41 -0700

    add additional jQuery interactive animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $(".clickable").click(function() {    
     $("#walrus-showing").toggle();
-    $("#walrus-hidden .clickable").toggle();
+    $("#walrus-hidden p").toggle();
+  });
+  
+   $(".fading-text").click(function() {
+    $("#fade-out-text").fadeOut();
+    $("#fade-out-text").fadeIn();
+  });
+  
+  $(".babyemu").click(function() {
+    $(".initially-hidden").slideToggle();
+    $(".initially-showing").slideToggle();
   });
 });
